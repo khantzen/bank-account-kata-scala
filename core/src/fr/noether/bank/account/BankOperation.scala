a56c95902855bf21585e2515842b17ce1e74d9cf
KHA <kevin.hantzen@gmail.com>
2020-10-20 15:47:41 +0200

    use closure for bank operations

---
+++ core/src/fr/noether/bank/account/BankOperation.scala
@@ -0,0 +1,13 @@
+package fr.noether.bank.account
+
+sealed trait BankOperation {
+  val apply: Amount => Amount
+}
+
+case class Deposit(amount: Amount) extends BankOperation {
+  override val apply: Amount => Amount = x => x.add(amount)
+}
+
+case class Withdrawal(amount: Amount) extends BankOperation {
+  override val apply: Amount => Amount = x => x.remove(amount)
+}
