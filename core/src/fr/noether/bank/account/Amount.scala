5615c9b708a00eb00f3ec37844dbbae26c564ad9
KHA <kevin.hantzen@gmail.com>
2020-10-20 14:05:55 +0200

    rearange order and refacto

--- core/src/fr/noether/bank/account/Amount.scala
+++ core/src/fr/noether/bank/account/Amount.scala
@@ -3,8 +3,6 @@ package fr.noether.bank.account
 case class Amount(value: BigDecimal)
 
 object Amount {
+  val ZERO: Amount = Amount.of(0)
   def of(value: Double) = new Amount(BigDecimal.valueOf(value))
-
-  val ZERO = Amount.of(0)
-
 }
--- core/src/fr/noether/bank/account/BankAccount.scala
+++ core/src/fr/noether/bank/account/BankAccount.scala
@@ -4,8 +4,6 @@ case class BankAccount(amount: Amount)
 
 object BankAccount {
   def startsWith(amount: Amount): BankAccount = BankAccount(amount)
-
   def freshAccount() = new BankAccount(Amount.ZERO)
-
 }
 
